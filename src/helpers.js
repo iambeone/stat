55d2750b72ec8df13201adb125bac47addd47b5e
Алексей Рудометов <iambeone@gmail.com>
2020-03-04 21:50:13 +0500

    initial

---
+++ src/helpers.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch')
+
+const graphQLQuery = ({ hasura_url, hasura_admin_key }) => async query => {
+  const data = await fetch(hasura_url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-hasura-admin-secret': hasura_admin_key
+    },
+    body: JSON.stringify({
+      query
+    })
+  }).then(res => res.json())
+
+  if (data.errors || data.error) {
+    console.error('Query failed:', query)
+    console.error('GraphQL query failed:', data.error || data.errors)
+    throw new Error('GraphQL query failed')
+  }
+
+  return data
+}
+
+module.exports = {
+    graphQLQuery: graphQLQuery(process.env.HASURA_URL, process.env.HASURA_ADMIN_KEY)
+}
