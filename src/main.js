e8dec1635fb78a860f30dce7ad2f312f086bfa21
Aleksei Rudometov <iambeone@gmail.com>
2020-03-10 16:53:47 +0500

    console.log removed

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(process.env)
+
 Vue.config.productionTip = false
 new Vue({
   render: h => h(App)
